c6ba3c310c04daba33c9c3e4e66b6e8866043e70
RuriSapphire <53013393+TamakiRuri@users.noreply.github.com>
2026-05-25 11:05:31 +0900

    Changed: command auto will also set up tts channel now

--- commands/auto.js
+++ commands/auto.js
@@ -15,7 +15,13 @@ module.exports = {
         const serverFile = bot_utils.get_server_file(interaction.guildId);
         const result = serverFile.autojoin;
         bot_utils.write_serverinfo(interaction.guildId, serverFile, {autojoin : !result});
-        //console.log('Command \' SetChannel \' used in Server '+ interaction.guild.toString() + ' ID :' + global.channelMap.get(interaction.guildId).channelId);
-        await interaction.reply({content:'自動入室を'+ ((!serverFile.autojoin) ? '有効': '無効') +'に設定しました。'})
+        if (!result === true){
+            bot_utils.write_serverinfo(interaction.guildId, serverFile, {textchannel:interaction.channel.id});
+            let connection = global.connections_map.get(interaction.guildId);
+            if (connection)connection.text = interaction.channelId;
+        }
+        logger.debug('Command \' SetChannel \' used in Server '+ interaction.guild.toString() + ' ID :' + global.channelMap.get(interaction.guildId).channelId);
+        const msg = '自動入室が'+ ((!result) ? '有効': '無効') +'になりました。' + (!result)? '読み上げのチャンネルを現在のチャンネルに設定しました。':' ';
+        await interaction.reply({content:msg})
     }
 }
